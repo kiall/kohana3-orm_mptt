0b8cc8058617005738cc2a9a09415eb1cf206b32
Kiall Mac Innes <kiall@managedit.ie>
2010-07-25 02:31:36 +0100

    Updating authors

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ORM_MPTT module for Kohana 3
+
+Authors:
+
+- Kiall Mac Innes
+- Mathew Davies
+- Mike Parkin
--- classes/orm/mptt.php
+++ classes/orm/mptt.php
@@ -1,9 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 /**
  * Modified Preorder Tree Traversal Class.
+ *
  * @author     Kiall Mac Innes
- * @copyright  (c) 2008-2010 Managed I.T.
- * @package    ORM MPTT
+ * @author     Mathew Davies
+ * @author     Mike Parkin
+ * @copyright  (c) 2008-2010
+ * @package    ORM_MPTT
  */
 abstract class ORM_MPTT extends ORM
 {
